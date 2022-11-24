07baca4fa132304569acf0295476c7d662f323ac
longyue0521 <longyueli0521@gmail.com>
2022-11-24 16:48:38 +0800

    feat: 添加InitClientProxy测试用例

---
+++ client/client_test.go
@@ -0,0 +1,69 @@
+package client_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/longyue0521/goRPC/client"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClient_Proxy(t *testing.T) {
+	testCases := map[string]struct {
+		service client.Service
+		wantErr error
+	}{
+		"nil": {
+			service: nil,
+			wantErr: client.ErrInvalidArgument,
+		},
+		"non pointer": {
+			service: Integer(1),
+			wantErr: client.ErrInvalidArgument,
+		},
+		"non pointer of struct": {
+			service: func() client.Service { a := Integer(1); return &a }(),
+			wantErr: client.ErrInvalidArgument,
+		},
+		"(*struct)(nil)": {
+			service: (*UserService)(nil),
+			wantErr: client.ErrInvalidArgument,
+		},
+		"user service": {
+			service: &UserService{},
+			wantErr: nil,
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			err := client.InitClientProxy(tc.service)
+			assert.ErrorIs(t, err, tc.wantErr)
+			if tc.wantErr != nil {
+				return
+			}
+			us, _ := tc.service.(*UserService)
+			_, _ = us.GetById(context.Background(), &GetByIdReq{})
+		})
+	}
+}
+
+type UserService struct {
+	GetById func(ctx context.Context, req *GetByIdReq) (*GetByIdResp, error)
+}
+
+type Integer int
+
+func (n Integer) Name() string {
+	return "Integer"
+}
+
+func (u *UserService) Name() string {
+	return "user-service"
+}
+
+type GetByIdReq struct {
+}
+
+type GetByIdResp struct {
+}
